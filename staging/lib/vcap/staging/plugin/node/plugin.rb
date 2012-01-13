cf8c09ddc78158d400215d560e5fe1bdd0ce5eb8
Jack Russell Software Company <jackhq@jackrussellsoftware.com>
2012-01-13 10:56:08 -0500

    Added NPM Rebuild Step!

--- staging/lib/vcap/staging/plugin/node/plugin.rb
+++ staging/lib/vcap/staging/plugin/node/plugin.rb
@@ -21,7 +21,10 @@ class NodePlugin < StagingPlugin
   private
   def startup_script
     vars = environment_hash
-    generate_startup_script(vars)
+    generate_startup_script(vars) do
+     cmds = ['cd app && npm rebuild >>../logs/npm.log 2>> ../logs/npm.log && cd ..;']
+     cmds.join('\n')
+    end
   end
 
   # TODO - I'm fairly sure this problem of 'no standard startup command' is
