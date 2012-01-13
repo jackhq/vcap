a92fc373f8079cc1664e84253db00cf7f23c5038
Tom Wilson <tom@jackhq.com>
2012-01-13 07:49:26 -0500

    Added npm rebuild command to rebuild the binaries of any node module before startup...

--- staging/lib/vcap/staging/plugin/node/plugin.rb
+++ staging/lib/vcap/staging/plugin/node/plugin.rb
@@ -23,7 +23,10 @@ class NodePlugin < StagingPlugin
 
   def startup_script
     vars = environment_hash
-    generate_startup_script(vars)
+    generate_startup_script(vars) do
+      cmds = ['cd app && npm rebuild >>../logs/npm.log 2>> ../logs/npm.log && cd ..;']
+      cmds.join('\n')
+    end
   end
 
   def stop_script
