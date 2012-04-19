cfe461ce37698a075fa5ee32726af6a9094fa6d8
P. Barrett Little <barrett@barrettlittle.com>
2012-04-18 21:08:59 -0400

    Router status endpoint is now configurable via the router cookbook.

--- dev_setup/cookbooks/router/attributes/default.rb
+++ dev_setup/cookbooks/router/attributes/default.rb
@@ -1,1 +1,5 @@
 default[:router][:config_file] = "router.yml"
+default[:router][:status_endpoint][:enabled] = false
+default[:router][:status_endpoint][:port] = '34503'
+default[:router][:status_endpoint][:user] = 'thin'
+default[:router][:status_endpoint][:password] = 'thin'
